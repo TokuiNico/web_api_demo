a4b9afb36d08c25985515c3e447c87632c10f64b
chyulin <vegetable80923@gmail.com>
2015-11-11 16:55:27 +0800

    Update ROI.py
    新增tra2seq和density

--- ROI.py
+++ ROI.py
@@ -7,6 +7,7 @@ from flask import request
 import psycopg2
 import requests
 import re
+import numpy as np
 
 conn_string = "host='127.0.0.1' dbname='adsl' user='adsl' password='radsl'"
 conn = psycopg2.connect(conn_string)
@@ -38,11 +39,12 @@ def list_ROI():
 @app.route('/datasets/ROI/<name>', methods=['GET'])
 def retrieve_ROI(name):
     cur = conn.cursor()
+    '''
     r = requests.get("http://127.0.0.1:5566/datasets/ROI")
     
     if name not in r.json()['datasets']:
         return error_request()
-
+    '''
     parameter = []
     # rid = rid
     if 'rid' in request.args:
@@ -87,6 +89,9 @@ def retrieve_ROI(name):
     try:
         cur.execute(query)
     except psycopg2.Error as e:
+        print "error:"+query
+        conn.rollback()
+        cur.close()
         return error_request()
 
     rows = [r for r in cur]
@@ -211,7 +216,156 @@ def delete_roi(name):
             cur.close()
             return(e.pgerror,400)
         return ("",200)
+
+#retrieve a dataset/trajectory/~in given range
+@app.route('/datasets/trajectory/<name>', methods=['GET'])
+def retrieve_a_dataset(name):
+    #conn_string = "host='192.168.100.200' dbname='hdwu' user='hdwu' password='4321'"
+    #conn = pg.connect(conn_string)
+    #if name == "taxi"
+    cur = conn.cursor()
     
+    args = request.args
+    trajectory = []
+    if 'tid' in args:
+        query = "select * from trajectory." + name + " where tid=" + args['tid']+ ";"
+        try:
+            cur.execute(query)
+        except psycopg2.Error as e:
+            conn.rollback()
+            cur.close()
+            return error_request()
+
+        rows = [r for r in cur]
+        for row in rows:
+            if len(trajectory) == 0:
+                trajectory.append({'tid': row[0], 'points': [{'index': row[1], 'lon': row[2], 'lat': row[3], 'timestamp': row[4]}]})
+            else: trajectory[0]['points'].append({'index': row[1], 'lon': row[2], 'lat': row[3], 'timestamp': row[4]})
+
+    elif 'lon_s'and 'lon_e' and 'lat_s' and 'lat_e' in args:
+        query = "select * from trajectory." + name + " where lon>=" + args['lon_s'] + " and lon<=" + args['lon_e'] + " and lat>=" + args['lat_s'] + " and lat<=" + args['lat_e'] + ";"
+        try:
+            cur.execute(query)
+        except psycopg2.Error as e:
+            conn.rollback()
+            cur.close()
+            return error_request()
+
+        rows = [r for r in cur]
+        for row in rows:
+            print row
+            if row[0] == len(trajectory)-1:                
+                trajectory[row[0]]['points'].append({'index': row[1], 'lon': row[2], 'lat': row[3], 'timestamp': row[4]})
+            else: trajectory.append({'tid': row[0], 'points': [{'index': row[1], 'lon': row[2], 'lat': row[3], 'timestamp': row[4]}]})
+
+    else:
+        query = "select * from trajectory." + name + ";"
+        try:
+            cur.execute(query)
+        except psycopg2.Error as e:
+            conn.rollback()
+            cur.close()
+            return error_request()
+
+        rows = [r for r in cur]
+        for row in rows:
+            if row[0] == len(trajectory)-1:
+                trajectory[row[0]]['points'].append({'index': row[1], 'lon': row[2], 'lat': row[3], 'timestamp': row[4]})
+            else: trajectory.append({'tid': row[0], 'points': [{'index': row[1], 'lon': row[2], 'lat': row[3], 'timestamp': row[4]}]})
+    return jsonify({'trajectory': trajectory})
+
+@app.route('/algo/tra2seq/<name>', methods=['GET'])
+def tra2seq(name):
+    #cur = conn.cursor()
+    
+    if not request.json or 'points' not in request.json:
+        return jsonify({'rid':[-1]})
+    
+    points = request.json.get("points","NULL");
+    
+    rid_list = []
+    
+    for point in points:
+        lat = point['lat'];
+        lon = point['lon'];
+        
+        if 'd' in request.args:
+            url = "http://127.0.0.1:5566/datasets/ROI/"+name+"?p="+str(lon)+","+str(lat)+"&d="+str(request.args['d'])
+        else:
+            url = "http://127.0.0.1:5566/datasets/ROI/"+name+"?p="+str(lon)+","+str(lat)
+        
+        r = requests.get(url)
+        
+        if not len(r.json()['ROI']): continue
+        
+        rid = r.json()['ROI'][0]['rid']
+        
+        print rid
+        
+        if(len(rid_list) == 0):
+            rid_list.append(rid)
+        elif(rid_list[-1] != rid):
+            rid_list.append(rid)
+
+    return jsonify({'seq':rid_list})
+   
+@app.route('/algo/density/<name>', methods=['GET'])
+def density(name):
+    if 'rid' not in request.args:
+        jsonify({"density":-1})
+    ROI = name
+    rid = request.args['rid']
+    payload = {'rid':str(rid)}
+    r = requests.get('http://127.0.0.1:5566/datasets/ROI/'+name+'?rid='+str(rid))
+    
+    
+    if not len(r.json()['ROI']): return jsonify({"density":-1})
+    
+    if r.json()['name'] == 'error' : return jsonify({"density":-1})
+    
+    range = r.json()['ROI'][0]['range']
+    payload = {'lon_s':str(range['west']), 'lon_e': str(range['east']),
+        'lat_s':str(range['south']), 'lat_e': str(range['north'])}
+    r = requests.get('http://127.0.0.1:5566/datasets/trajectory/'+name,params=payload)
+    
+    count = len(r.json()['trajectory'])
+
+    return jsonify({"density":count})
+
+    
+'''{}
+    input: a list of ROI sequence
+    {
+        "TRD": [
+            [R1,R2,R3...],
+            [R10,R11,R12...]
+        ]
+    }
+    
+    output: UMG
+    {
+        "UMG": [
+            [0,1,2,3],
+            [1,2,3,4],
+            [2,3,4,5]
+        ]
+    }
+'''
+'''
+@app.route('/algo/umg',methods=['GET'])
+def umg():
+    if not request.json or 'TRD' not in request.json:
+        return jsonify({'UMG':[]})
+    datasets = request.json['TRD']
+    
+    
+    max_rid = 0
+    for seq in datasets:
+        for roi in seq:
+            max_rid = roi if roi > max_rid else max_rid
+    umg = np.zeros(max_rid,max_rid)
+'''    
+        
 def error_request():
     return jsonify({'name':'error','ROI':[{'rid':-1,'density':-1,'score':-1,'range':{'west':'0','east':'0','south':'0','north':'0'}}]})
 
